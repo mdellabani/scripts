9fe1ecb11859003d798a13dc8cb3e40c9fd55265
mdellabani <mdellabani@gmail.com>
2023-07-27 10:14:44 +0200

    Initial commit.

---
+++ config/nnn.sh
@@ -0,0 +1,6 @@
+export NNN_PLUG='f:finder;o:fzopen;c:fzcd;p:preview-tui;d:diffs;t:nmount;v:imgview;r:gitroot'
+BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+export NNN_FIFO=/tmp/nnn.fifo
+alias nnn="nnn -e -d"
+
